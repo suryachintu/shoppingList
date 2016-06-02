b642b4af9b6ce5743c432de5337378374567e12f
suryachintu <suryaparsa@gmail.com>
2016-06-02 14:18:23 +0530

    Initial commit Found bug , gradle properties not found

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/utils/Constants.java
@@ -0,0 +1,28 @@
+package com.udacity.firebase.shoppinglistplusplus.utils;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    /**
+     * Constants related to locations in Firebase, such as the name of the node
+     * where active lists are stored (ie "activeLists")
+     */
+
+
+    /**
+     * Constants for Firebase object properties
+     */
+
+
+    /**
+     * Constants for Firebase URL
+     */
+        public static final String FIREBASE_URL = "https://shoplistplusplus-ffda2.firebaseio.com/";
+    /**
+     * Constants for bundles, extras and shared preferences keys
+     */
+
+
+}
